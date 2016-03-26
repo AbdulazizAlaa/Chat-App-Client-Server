dec2c980eb3e9e3dd97aae279ab34a3465595131
Abdulaziz Alaa <abdulazizalaa@ymail.com>
2016-03-26 13:30:17 +0200

    Adding Client App

---
+++ Client-app/src/Client.java
@@ -0,0 +1,171 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Client {
+	
+	public String serverIp = "localhost";
+	public String serverValidation = "ok i know you ";
+	public int port;
+	public String username;
+	public String response;
+	Socket socket;
+	public DataOutputStream serverOut;
+	public DataInputStream serverIn;
+	
+	
+	public Client() {
+		// TODO Auto-generated constructor stub
+		this("192.168.1.4", 7777, "aziz");
+	}
+
+	public Client(String username) {
+		// TODO Auto-generated constructor stub
+		this("192.168.1.4", 7777, username);
+	}
+	
+	public Client(String Ip, int port, String username) {
+		// TODO Auto-generated constructor stub
+		this.serverIp = Ip;
+		this.port = port;
+		this.username = username;		
+	}
+	
+	public boolean connect() throws UnknownHostException, IOException{
+		socket = new Socket(serverIp, port);
+		
+		serverOut = new DataOutputStream(socket.getOutputStream());
+		serverIn = new DataInputStream(socket.getInputStream());
+		
+		serverOut.writeUTF(username);
+		serverOut.flush();
+		
+		response = serverIn.readUTF();
+		
+		if(!response.equals(serverValidation)){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	public boolean Close() throws IOException{
+		
+		serverIn.close();
+		serverOut.close();
+		socket.close();
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame w = new Window("Mighty Chat", 500, 500);
+		
+//		String ip = "localhost";
+//		String username;
+//		String response;
+//		DataOutputStream serverOut;
+//		DataInputStream serverIn;
+//		Scanner in = new Scanner(System.in);
+//		
+//		System.out.println("Enter your name to login.");
+//		
+//		username = in.next();
+		
+//		System.out.println("Enter IP Address of server.");
+//		String temp = in.next();
+//		if(!temp.equals("1")){
+//			ip = temp;
+//		}
+//		ip = "192.168.1.4";
+		
+//		try {
+//			Socket socket = new Socket(ip, 7777);
+//			
+//			serverOut = new DataOutputStream(socket.getOutputStream());
+//			serverIn = new DataInputStream(socket.getInputStream());
+//			
+//			serverOut.writeUTF(username);
+//			serverOut.flush();
+//			
+//			response = serverIn.readUTF();
+//			
+//			if(!response.equals("ok")){
+//				
+//			}
+//			
+//			Thread inputThread = new Thread(new Runnable() {
+//				
+//				@Override
+//				public void run() {
+//					// TODO Auto-generated method stub
+//					String inputStr;
+//					String[] messages;
+//					
+//					while(true){
+//						try {
+//							inputStr = serverIn.readUTF();
+//							messages = inputStr.split(",");
+//							
+//							System.out.println(messages[0]+": "+messages[1]);
+//						} catch (IOException e) {
+//							// TODO Auto-generated catch block
+////							System.out.println("Error: Message was not recieved");
+//						}
+//						
+//						
+//					}
+//				}
+//			});
+//			inputThread.start();
+//			
+//			Thread outputThread = new Thread(new Runnable() {
+//				
+//				@Override
+//				public void run() {
+//					// TODO Auto-generated method stub
+//					String userinput;
+//					String message;
+//					while(true){
+//						userinput = in.next();
+//						if(userinput.equals("q")){
+//							try {
+//								serverOut.close();
+//								serverIn.close();
+//								socket.close();
+//							} catch (IOException e) {
+//								// TODO Auto-generated catch block
+//								System.out.println("Error client ended unexpectedly.");
+//							}
+//							break;
+//						}else{
+//							message = username + "," + userinput;
+//							try {
+//								serverOut.writeUTF(message);
+//							} catch (IOException e) {
+//								// TODO Auto-generated catch block
+//								System.out.println("Error message could not be sent.");
+//							}
+//						}
+//					}
+//				}
+//			});
+//			outputThread.start();
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+////			e.printStackTrace();
+//			System.out.println("Server did not accept connection. Server is out of reach.");
+//		}
+	}
+
+}
---
+++ Client-app/src/Window.java
@@ -0,0 +1,128 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
+
+public class Window extends JFrame{
+	
+	Client client;
+	String title;
+	String username;
+	int w, h;
+	
+	JButton loginB;
+	JButton logoutB;
+	JLabel UserLabel;
+	JLabel statusLabel;
+	JList<String> usersList;
+	UsersModel listModel;
+	JTextArea chatArea;
+	JButton sendB;
+	
+	public Window(String title, int w, int h) {
+		// TODO Auto-generated constructor stub
+		
+		this.title = title;
+		this.username = "USER";
+		this.w = w;
+		this.h = h;
+		JFrame frame = this;
+		listModel = new UsersModel();
+		listModel.users.add("aziz");
+		
+		this.setTitle(title);
+		this.setSize(w, h);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setLayout(new FlowLayout());
+		
+		loginB = new JButton("Login");
+		this.add(loginB);
+		
+		JPanel mainPanel = new JPanel(new GridLayout(3, 2, 50, 20));
+		mainPanel.setVisible(false);
+		
+		logoutB = new JButton("Logout");
+		sendB = new JButton("Send");
+		UserLabel = new JLabel("");
+		statusLabel = new JLabel("");
+		usersList = new JList<String>(listModel);
+		chatArea = new JTextArea(5, 10);
+				
+		mainPanel.add(logoutB);
+		mainPanel.add(UserLabel);
+		mainPanel.add(usersList);
+		mainPanel.add(chatArea);
+		mainPanel.add(sendB);
+		mainPanel.add(statusLabel);
+		
+		this.add(mainPanel);
+		
+		logoutB.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				frame.setTitle(title);
+				loginB.setVisible(true);
+				mainPanel.setVisible(false);
+			}
+		});
+		
+		loginB.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String name = (String) JOptionPane.showInputDialog(frame, "Enter Username:", "Username", JOptionPane.PLAIN_MESSAGE);
+				if((name != null) && (name.length() > 0)){
+					username = name;
+				}else{
+					username = "USER";
+				}
+				
+				frame.setTitle("User-->"+username);
+				UserLabel.setText(username);
+				loginB.setVisible(false);
+				mainPanel.setVisible(true);
+				
+				
+				client = new Client(username);
+				
+				try {
+					if(client.connect()){
+						System.out.println("connected");
+						statusLabel.setText("Connected");
+					}else{
+						logoutB.doClick();
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+		
+		
+		
+		
+	}
+	
+}
